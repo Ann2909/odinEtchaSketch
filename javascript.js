09dd87cabd7b93e93b1f61067d43431ed6b601e2
an2909 <149702668+Ann2909@users.noreply.github.com>
2024-06-13 13:22:53 +0700

    Make hover effect to change color a function and drawing squares a function

--- javascript.js
+++ javascript.js
@@ -1,40 +1,25 @@
 //Create a webpage with a 16x16 grid of square divs.
-let container = document.querySelector('#container');
-let header1 = document.querySelector('h1');
 let body = document.querySelector('#body');
+let header1 = document.querySelector('h1');
+let container = document.querySelector('#container');
+let redrawBtn = document.querySelector('.redrawBtn');
+let newBtn = document.querySelector('.newBtn');
 
 let squareDivs = [];
-for (let i = 0; i < 16; i++) {
-    let eachSquareRow = document.createElement('div');
-    eachSquareRow.classList.add('row');
-    
-    for (let i = 0; i < 16; i++) {
-        // Create a new div element
-        let oneSquare = document.createElement('div');
-        squareDivs.push(oneSquare);
-        oneSquare.classList.add('square');
 
-        // Append the square div to each row
-        eachSquareRow.appendChild(oneSquare);
-    }
-
-    container.appendChild(eachSquareRow);
-}
+drawNewGrid(16, container);
+setUpHoverEffect();
 
 //Set up a hover effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
-for (let div of squareDivs) {
-    div.addEventListener('mouseenter', (e) => {
-        div.style.backgroundColor = 'black';
-    });
-} 
+function setUpHoverEffect() {
+    for (let div of squareDivs) {
+        div.addEventListener('mouseenter', (e) => {
+            div.style.backgroundColor = 'black';
+        });
+    } 
+}
 
-//Add a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. 
 //First, let's have a button that will redraw
-let redrawBtn = document.createElement('button');
-redrawBtn.textContent = 'Click to redraw';
-
-body.insertBefore(redrawBtn, header1);
-
 redrawBtn.addEventListener('click', (e) => {
     for (let div of squareDivs) {
         div.style.backgroundColor = 'white';
@@ -42,17 +27,41 @@ redrawBtn.addEventListener('click', (e) => {
 });
 
 //Let's have a button that will draw a new sketch
-let newBtn = document.createElement('button');
-newBtn.textContent = 'Click to draw a new one';
-body.insertBefore(newBtn, header1);
-
 newBtn.addEventListener('click', (e) => {
-    let newGrid = prompt('How many squares do you want per row?', '16');
     body.removeChild(container);
+    let newContainer = document.createElement('div');
+    newContainer.setAttribute('id', 'newcontainer');
+    body.appendChild(newContainer);
+
+    let newGrid = prompt('How many squares do you want per row?', '16');
+    
+    //remove all references to the old divs that have been removed
+    while (squareDivs.length > 0) {
+        squareDivs.pop();
+    }
+    drawNewGrid(newGrid, newContainer);
+    //set up hover effect for new grid
+    setUpHoverEffect();
 });
 
-//Once entered, the existing grid should be removed, and a new grid should be generated in the same total space as before (e.g., 960px wide) so that you've got a new sketch pad.
-//Tip: Set the limit for the user input to a maximum of 100. A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
-//Research button tags in HTML and how you can make a JavaScript function run when one is clicked.
-//Also check out prompts.
-//You should be able to enter 64 and have a brand new 64x64 grid pop up without changing the total amount of pixels used.
+//function to create a grid of as many squares as the user wants
+function drawNewGrid(squareNum, holder) {
+    let refSquareDivs = [];
+    for (let i = 0; i < squareNum; i++) {
+        let eachSquareRow = document.createElement('div');
+        eachSquareRow.classList.add('row');
+        
+        for (let j = 0; j < squareNum; j++) {
+            // Create a new div element
+            let oneSquare = document.createElement('div');
+            oneSquare.classList.add('square');
+            refSquareDivs.push(oneSquare);
+    
+            // Append the square div to each row
+            eachSquareRow.appendChild(oneSquare);
+        }
+    
+        holder.appendChild(eachSquareRow);
+    }
+    squareDivs = refSquareDivs;
+}
